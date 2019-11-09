1a9dbfd984117f002b9c4fd2a9306f57a22af60e
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-09 07:18:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,8 +24,8 @@ def retrieval
 
 end
 
-def adding
-  shipping_manifest = {
+def 
+   = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
