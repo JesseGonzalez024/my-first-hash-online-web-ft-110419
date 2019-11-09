a1d3e77ef15920604fbddf46a39ecb093240a4d1
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-09 07:21:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,8 +13,8 @@ def shipping_manifest
 
 end
 
-def 
-   = {
+def shipping_manifest
+  the_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
@@ -24,20 +24,20 @@ def
 
 end
 
-def shipping_manifest
-   the_manifest= {
+def retrieval
+   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+  shipping_manifest[:"muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  shipping_manifest[:"gun powder"] = 4
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
